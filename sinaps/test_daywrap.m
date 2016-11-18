684eed0b4ce284325b056317784aa096ee34001f
Oleksandr Novychenko <novychenko@evologics.de>
2016-11-18 10:38:58 +0100

    small optimization

--- sinaps/test_daywrap.m
+++ sinaps/test_daywrap.m
@@ -1,9 +1,9 @@
 function test_daywrap(date1, time1, time2)
-    t1 = utc2timestamp(time1);
+    t1 = utc2timestamp(time1, date1);
     t2 = utc2timestamp(time2);
 
     diff = wrap_daydiff(t2 - t1);
-    [ rtime, rdate ] = timestamp2utc(utc2timestamp(time1, date1) + diff);
+    [ rtime, rdate ] = timestamp2utc(t1 + diff);
 
     disp('Difference [days]:'), disp(floor(diff / 86400 + 0.5));
     disp('Difference [seconds]:'), disp(diff);
